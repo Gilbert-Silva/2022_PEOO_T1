0724a89a0951466987a59bb87b8cb17dd2900c75
GilbertAzevedo <gilbertif1995@outlook.com>
2022-08-29 13:06:47 +0000

    Exercícios de classes

---
+++ Cap_0829/exemplo.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Console.WriteLine("Qual a distância em km percorrida na sua viagem?");
+    double dist = double.Parse(Console.ReadLine());
+    Console.WriteLine("Quanto tempo você gastou (hh:mm:ss)?"); // "02:30" - 2h e 30 min
+    string tempo = Console.ReadLine();
+    string[] v = tempo.Split(':'); // "02" "45"
+    int h = int.Parse(v[0]);       // Número de horas:   "02" -> 2
+    int m = int.Parse(v[1]);       // Número de minutos: "30" -> 30
+    int s = int.Parse(v[2]);       // Número de segundos: "30" -> 30
+    double horas = h + m/60.0 + s/3600.0;     // Total de horas:    2,5 horas
+    
+    Viagem minhaViagem = new Viagem();
+    minhaViagem.SetDistancia(dist);
+    minhaViagem.SetTempo(horas);
+    Console.WriteLine($"Distância percorrida: {minhaViagem.GetDistancia()} km");
+    Console.WriteLine($"Tempo gasto: {minhaViagem.GetTempo()} h");
+    Console.WriteLine($"Velocidade média: {minhaViagem.VelocidadeMedia()} km/h");
+  }
+}
+
+class Viagem {
+  private double distancia;
+  private double tempo = 1;
+  public void SetDistancia(double d) {
+    if (d > 0) distancia = d;
+  }
+  public void SetTempo(double t) {
+    if (t > 0) tempo = t;
+  }
+  public double GetDistancia() {
+    return distancia;
+  }
+  public double GetTempo() {
+    return tempo;
+  }
+  public double VelocidadeMedia() {
+    return distancia / tempo;
+  }
+}
